404b8e3813304a4474cd28b2aeede1190dfd53fd
Etienne van de bijl <etiennevandebijl@outlook.com>
2021-08-03 13:16:38 +0200

    WIP

--- ML/Transfer/interpret_results.py
+++ ML/Transfer/interpret_results.py
@@ -5,75 +5,75 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 from project_paths import get_results_folder
+from ML.Transfer.experimental_setup import NAMES
+NAMES_ = {y: x for x, y in NAMES.items()}
 
-input_path = get_results_folder("CIC-IDS-2017", "BRO", "2_Preprocessed_DDoS", "Supervised") + "Train-Test 0/Paper/http-tcp/"
-
-NAMES = {"DoS - Hulk": "Hulk",
-         "DDoS - HOIC": "HOIC",
-         "DDoS - LOIC": "LOIC",
-         "DDoS - LOIC - HTTP": "HLOIC",
-         "DDoS - LOIC - UDP": "ULOIC",
-         "DoS - SlowHTTPTest": "Test",
-         "DoS - GoldenEye": "Eye",
-         "DDoS - Botnet": "Bot",
-         "DoS - Slowloris": "Slow"}
-NAMES2 = {y: x for x, y in NAMES.items()}
-
+#%% 
+input_path = get_results_folder("CIC-IDS-2017", "BRO", "2_Preprocessed_DDoS",
+                                "Supervised") + "Train-Test 0/Paper/http-tcp/"
 
 results = []
 for file in glob.glob(input_path + '**/scores.csv', recursive=True):
     tags = file.split(os.sep)
-    test_attack = tags[-4]
-    train_attacks = tags[-3].split(" ")
-
-    train_attacks = [NAMES2[l] for l in train_attacks]
-
     model = tags[-2]
+    train_attacks = tags[-3].split(" ")
+    test_attack = tags[-4]
+
+    train_attacks = [NAMES_[l] for l in train_attacks]
     number_of_attacks = len(train_attacks)
 
     df = pd.read_csv(file, sep=";", decimal=",", index_col=0).fillna(0)
+    
     f1 = df.loc["Malicious", "F1 Score"]
     n = df[["Benign", "Malicious"]].sum().sum()
     f1_b = df.loc["Malicious", "F1 Baseline"]
-    #if test_attack not in train_attacks:
-    results.append([test_attack, len(train_attacks), str(train_attacks), 
+    
+    results.append([test_attack, len(train_attacks), str(train_attacks),
                         model, f1, n, round(f1_b,3)])
 
 df = pd.DataFrame(results, columns = ["Test", "Number of trained D(D)oS attacks",
                                       "Train", "Model", "F1", 'n', "F1 Baseline"])
-df = df[df["Number of trained D(D)oS attacks"] == 1]
 
-results2 = {}
-for test_attack, group in df.groupby("Test"):
-    pivot = pd.pivot_table(group, values = "F1",
-                           index = ["Train", "Number of trained D(D)oS attacks", 
-                                    "n", "F1 Baseline"], columns="Model")
-    results2[test_attack] = pivot
+# %%
+
+df_ = df[df["Number of trained D(D)oS attacks"] == 1]
+
+# %%
+
+group_test = {}
+for test_attack, group in df_.groupby("Test"):
+    group_test[test_attack] = pd.pivot_table(group, values = "F1", columns="Model",
+                                           index = ["Train"])
 
 
-df_list = []
-for test_attack, pivot in results2.items():
-    tab = pivot.reset_index()[["Train","DT"]]
-    tab["Test"] = test_attack
-    tab["Train"] = tab["Train"].map(lambda x: x.replace("['","").replace("']",""))
-    df_list.append(tab)
+# %%
 
-df_new = pd.concat(df_list)
-df_new = df_new[["Train","Test","DT"]].reset_index(drop=True)
-df_new = df_new[df_new["Test"] != "Malicious"]
+group_model = {}
+for model, group in df_.groupby("Model"):
+    group_ = group[["Train","Test","F1"]]
+    group_ = group_[group_["Test"] != "Malicious"]
+    group_["Train"] = group_["Train"].map(lambda x: x.replace("['",
+                                                              "").replace("']",
+                                                                          ""))
+    group_["F1"] = group_["F1"].round(3)
+    group_model[model] = group_
 
-df_new = df_new[df_new["DT"] > 0]
-df_new["DT"] = df_new["DT"].round(3)
-G = nx.from_pandas_edgelist(df_new,source='Train',
-                                   target='Test',edge_attr=True,
-                                   create_using=nx.DiGraph())
 
-plt.figure(figsize = (15,15))
+# %%
+
 cmap = plt.get_cmap('YlOrRd')
-pos=nx.spring_layout(G)
-colors = [cmap(G[u][v]['DT']) for u,v in G.edges()]
+pos = None
+for model, group in group_model.items():
 
-nx.draw_networkx(G, pos, edge_color=colors, connectionstyle="arc3,rad=0.1" )
-#nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-plt.title("CIC-IDS-2017 HTTP-TCP Decision Tree F1 score as Directed Graph")
-plt.show()
+    plt.figure(figsize = (15,15))
+
+    G = nx.from_pandas_edgelist(group,source='Train', target='Test',
+                            edge_attr=True, create_using=nx.DiGraph())
+
+    if pos == None:
+        pos=nx.spring_layout(G)
+    colors = [cmap(G[u][v]['F1']) for u,v in G.edges()]
+
+    nx.draw_networkx(G, pos, edge_color=colors, connectionstyle="arc3,rad=0.1")
+    plt.title(model)
+    plt.show()
