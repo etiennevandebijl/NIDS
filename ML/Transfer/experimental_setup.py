ea842b7bb90372723c965d9bb1742862cdce3c4d
Etienne van de Bijl <etiennevandebijl@outlook.com>
2021-08-31 22:47:24 +0200

    WIP

--- ML/Transfer/experimental_setup.py
+++ ML/Transfer/experimental_setup.py
@@ -96,7 +96,7 @@ def compute_transfer_learning(df_train, df_test, output_path):
         select_train_labels(df_train, df_test_, None, output_path_m)
 
 def main_clf_sl(experiment, version, protocols):
-    RS = 3
+    RS = 0
     data_path = get_data_folder(experiment, "BRO", version) + "Train-Test " + str(RS) +"/"
     output_path = get_results_folder(experiment, "BRO", version, "Supervised") + \
                                      "Train-Test " + str(RS) +"/Paper/"
--- ML/Transfer/models.py
+++ ML/Transfer/models.py
@@ -6,31 +6,32 @@ from sklearn.tree import DecisionTreeClassifier
 # Sklearn must be version 0.23.2 in stead of 0.24.2, otherwise it takes ages
 
 models = {
-    "GNB": {"clf": GaussianNB(),
-            "param": {"priors": [None]}
-            }
-    ,
-    "DT": {"clf": DecisionTreeClassifier(),
-            "param": {"criterion": ["gini","entropy"],
-                      "splitter": ["best", "random"],
-                      "class_weight": [None, "balanced"],
-                      "max_features": ["auto", None, "sqrt", "log2"],
-                      "random_state": [0]}
-            }
+    # "GNB": {"clf": GaussianNB(),
+    #         "param": {"priors": [None]}
+    #         }
     # ,
-    # "RF": {"clf": RandomForestClassifier(),
-    #         "param": {"criterion": ["gini", "entropy"],
-    #                   "class_weight": [None],
-    #                   "max_features": ["auto"],
-    #                   "n_estimators": [5, 10, 100],
-    #                   "n_jobs": [-1],
+    # "DT": {"clf": DecisionTreeClassifier(),
+    #         "param": {"criterion": ["gini","entropy"],
+    #                   "splitter": ["best", "random"],
+    #                   "class_weight": [None, "balanced"],
+    #                   "max_features": ["auto", None, "sqrt", "log2"],
     #                   "random_state": [0]}
     #         }
+    # ,
+    "RF": {"clf": RandomForestClassifier(),
+            "param": {"criterion": ["gini", "entropy"],
+                      "class_weight": [None, "balanced"],
+                      "max_features": ["auto"],
+                      "n_estimators": [10, 50, 100, 250],
+                      "n_jobs": [-1],
+                      "random_state": [0]}
+            }
     # ,
     # "KNN": {"clf": KNeighborsClassifier(),
     #         "param": {"n_neighbors": [1],
     #                   "weights": ["uniform"],
-    #                   # "algorithm":["ball_tree", "kd_tree"],
+    #                   "algorithm":["ball_tree", "kd_tree"],
+    #                   "leaf_size":[10, 20,],
     #                   "n_jobs": [-1]}
     #         }
     # ,
