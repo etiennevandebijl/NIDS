d2f6f67ac3f28ca42e31ab62d6734d24077eed60
Etienne van de Bijl <etiennevandebijl@outlook.com>
2022-07-19 14:12:18 +0200

    REMOVE DUTCH DRAW METHODS

--- ML/Transfer/models.py
+++ ML/Transfer/models.py
@@ -1,33 +1,33 @@
 from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 # Sklearn must be version 0.23.2 in stead of 0.24.2, otherwise it takes ages
 
 models = {
-    # "GNB": {"clf": GaussianNB(),
-    #         "param": {"priors": [None],
-    #                   "var_smoothing":[1e-200]} #Dit is kritical
-    #         }
-    # ,
-    # "DT": {"clf": DecisionTreeClassifier(),
-    #         "param": {"criterion": ["gini","entropy"],
-    #                   "splitter": ["best", "random"],
-    #                   "class_weight": [None, "balanced"],
-    #                   "max_features": ["auto", None, "sqrt", "log2"],
-    #                   "random_state": [0]}
-    #         }
-    # ,
-    # "RF": {"clf": RandomForestClassifier(),
-    #         "param": {"criterion": ["gini", "entropy"],
-    #                   "class_weight": [None, "balanced"],
-    #                   "max_features": ["auto"],
-    #                   "n_estimators": [10, 50, 100, 250],
-    #                   "n_jobs": [-1],
-    #                   "random_state": [0]}
-    #         }
-    # ,
+    "GNB": {"clf": GaussianNB(),
+            "param": {"priors": [None],
+                      "var_smoothing":[1e-200]} 
+            }
+    ,
+    "DT": {"clf": DecisionTreeClassifier(),
+            "param": {"criterion": ["gini","entropy"],
+                      "splitter": ["best", "random"],
+                      "class_weight": [None, "balanced"],
+                      "max_features": ["auto", None, "sqrt", "log2"],
+                      "random_state": [0]}
+            }
+    ,
+    "RF": {"clf": RandomForestClassifier(),
+            "param": {"criterion": ["gini", "entropy"],
+                      "class_weight": [None, "balanced"],
+                      "max_features": ["auto"],
+                      "n_estimators": [10, 50, 100, 250],
+                      "n_jobs": [-1],
+                      "random_state": [0]}
+            }
+    ,
     "KNN": {"clf": KNeighborsClassifier(),
             "param": {"n_neighbors": [5],
                       "weights": ["uniform"],
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # NIDS
 
-This github contains the code of the article ``Detecting Variants of Application Layer DoS/DDoS Attacks using Machine Learning" by Van de Bijl et al. 2021. 
+This github contains the code of the article ``Detecting Novel Variants of Application Layer (D)DoS Attacks using Supervised Learning" by Van de Bijl et al. 2022.
 
 The workflow starts in the Zeek folder with the extraction of log files by Zeek. After extracting from the raw pcap files, other datasets can be created using the files in the other folder in Zeek.
-
